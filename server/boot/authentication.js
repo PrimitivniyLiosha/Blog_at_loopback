34fb2dc82f6c5fcf2a19b3e9fce7713a67954ab0
Leshen <lex.frol2020@gmail.com>
2019-04-08 11:48:17 +0300

    Creating relationship between models and connecting MongoDb

--- server/boot/authentication.js
+++ server/boot/authentication.js
@@ -1,6 +1,4 @@
-'use strict';
-
-module.exports = function enableAuthentication(server) {
+module.exports.enableAuthentication = (server) => {
   // enable authentication
   server.enableAuth();
 };
